3689ff48762607348753b65f8df8f44a5971de71
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2021-07-13 15:48:29 -0500

    updates

--- Model/Washing Model Trial.py
+++ Model/Washing Model Trial.py
@@ -19,11 +19,8 @@ from matplotlib import pyplot as plt
 
 
 
-TrialDF = gb2[0]
-Agg_WashTime =0
-WashT_Per_Pallet = 3 #min
-Agg_WashTime = Agg_WashTime+WashT_Per_Pallet
 
+'''
 #Setup
 Time_Wash=Agg_WashTime
 Times = np.arange(0, Time_Wash+0.1, 0.1).tolist()
@@ -43,35 +40,55 @@ r1= 12.75 #(mg/(ml/min**2))
 r2 = 7.47 #(mg/(ml/min**2))
 r3 = 5.56 #(mg/(ml/min**2))
 
-L_L = []
-for i in Times: 
-    Running_Time = i
-    Time_Interval = Running_Time-(Pre_runningT)
-    print(Time_Interval)
-    if 0<= Running_Time <= (0+Ro0) :
-        Rate = r1
-        X = 1
-        print("Yes")
-    elif Ro <= Running_Time <= (Ro+Ro0) :
-        Rate = r2
-        X = 1
-    elif 2*Ro <= Running_Time <= (2*Ro+Ro0) : 
-        Rate = r3
-        X = 1
-    else: 
-        X = 0
-    dO = K0*Time_Interval
-    O = O+dO
+'''
 
-    decay = ((-SigC*Time_Interval)*C) - ((BC*Time_Interval)*O*C) 
-    Increase = (Rate*X*Time_Interval)
-    dC = decay + Increase
-    C = C+dC
-    L_L.append(C)
-    Pre_runningT = i
-print(C)    
+V = 890 #L
 
-plt.plot(L_L)
+def F_Chloride_lvl (Time_Wash):
+    Times = np.arange(0, Time_Wash+0.1, 0.1).tolist()
+    Times = [round(num, 1) for num in Times]
+    #Addition Rates
+    r1= 12.75 #(mg/(ml/min**2))
+    r2 = 7.47 #(mg/(ml/min**2))
+    r3 = 5.56 #(mg/(ml/min**2))
+    #Dose
+    Ro = 12 #Chlorine dosing period, ever 12 minutes
+    Ro0 = 2 #Minutes duration of dose
+    #Time
+    Pre_runningT = 0
+    K0 = 32.3 # free chroliride demand per min
+    C= 0 # ini #(mg/L) #Concentration of Free Chrloride available
+    O = 0  # Initial Oxygen deman
+    #Other parameters
+    SigC = 1.70*(10**-3)
+    BC = 5.38*(10**-4)
+    for i in Times: 
+        Running_Time = i
+        Time_Interval = Running_Time-(Pre_runningT)
+        if 0<= Running_Time <= (0+Ro0) :
+            Rate = r1
+            X = 1
+        elif Ro <= Running_Time <= (Ro+Ro0) :
+            Rate = r2
+            X = 1
+        elif 2*Ro <= Running_Time <= (2*Ro+Ro0) : 
+            Rate = r3
+            X = 1
+        else: 
+            X = 0
+        dO = K0*Time_Interval
+        O = O+dO
+        decay = ((-SigC*Time_Interval)*C) - ((BC*Time_Interval)*O*C) 
+        Increase = (Rate*X*Time_Interval)
+        dC = decay + Increase
+        C = C+dC
+        Pre_runningT = i
+    return C
+
+C = F_Chloride_lvl(Time_Wash= 0.2)
+
+
+
 TrialDF["CFU"] = 0
 TrialDF.at[0,"CFU"] = 3000
 
@@ -79,6 +96,12 @@ TrialDF.at[0,"CFU"] = 3000
 Xw =0  #pathogen in process water MPN/ml
 Xl = 0
 WaterCont_List = []
+
+
+TrialDF = gb2[0]
+Agg_WashTime =0
+WashT_Per_Pallet = 3 #min
+Agg_WashTime = Agg_WashTime+WashT_Per_Pallet
 
 for i, row in TrialDF.iterrows():
     AvCont = TrialDF.at[i,"CFU"] /(TrialDF.at[i,"Weight"]*454)
@@ -108,6 +131,9 @@ print(WaterCont_List)
 WaterCont_List
 
 
+
+
+#%%
 
 for i in Times: 
     Running_Time = i
