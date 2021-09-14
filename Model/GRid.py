9732c9bc19bfa0db9c0032ef21cb80f024a4a682
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2021-09-14 07:33:54 -0500

    updates

---
+++ Model/GRid.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 16:07:18 2021
+
+@author: Gustavo Reyes
+"""
+
+import math
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+2000/5
+2000/2
+
+math.sqrt(400)
+
+d = pd.DataFrame(np.zeros((20, 100)))
+
+d.at[13,40] = 50
+ax =sns.heatmap(d,cmap="YlGnBu")
+ax.set_aspect("equal")
