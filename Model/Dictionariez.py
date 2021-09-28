2e323b303e7a2a20e73ad486212e83efd0d8a0e8
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2021-09-28 16:29:29 -0500

    CPS-Update

--- Model/Dictionariez.py
+++ Model/Dictionariez.py
@@ -4,4 +4,38 @@ Created on Mon Sep 27 11:31:26 2021
 
 @author: gareyes3
 """
+import math
+import pandas as pd
 
+Niteration = 20
+i = 3
+
+data_contprog = {"Initial":[""]*Niteration,
+             "Bef Pre-Harvest Samp": [""]*Niteration,
+             "Aft Pre-Harvest Samp": [""]*Niteration,
+             "Bef Harvest Samp":[""]*Niteration,
+             }
+
+df_contprog = pd.DataFrame(data_contprog)
+
+df_contprog.at[i,"Initial"]  =3
+
+[""]*Niteration
+
+
+def Output_Collection_Prog(df, outputDF, Step_column):
+    #df= main model df
+    #outputDF = contprogdataframe
+    #Step_column = column for the step we are at
+    Total_CFU = sum(df.CFU)
+    outputDF.at[i,Step_column] = Total_CFU
+    return outputDF
+
+
+def Output_Collection_Final(df, outputDF, Step):
+    if Step == "PH":
+        
+
+
+
+        
