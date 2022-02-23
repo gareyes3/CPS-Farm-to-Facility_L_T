e8defeef359a6cb0b34447a9db98969b83ccc674
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2022-02-23 15:40:43 -0600

    Update InitialContaminationWorking.py

--- Model/InitialContaminationWorking.py
+++ Model/InitialContaminationWorking.py
@@ -10,14 +10,18 @@ import numpy as np
 import SCInputz
 import random
 import funz
+from scipy.stats import beta
+
 
 #Assisting Functions
 def pert(a, b, c, *, size=1, lamb=4):
     r = c - a
     alpha = 1 + lamb * (b - a) / r
     beta = 1 + lamb * (c - b) / r
-    return a + np.random.beta(alpha, beta, size=size) * r
+    return float(a + np.random.beta(alpha, beta, size=size) * r)
 
+def betagen(a,b,mini,maxi):    
+    return float(beta.rvs(a , b)* (maxi - mini) + mini)
 
 
 #Contamination from irrigation water pang et al
@@ -41,7 +45,59 @@ days_picked = [i for i in days if probability_irrigation_day>np.random.uniform(0
 F_InitialCont()
 
 
+
+
+
+#Scenario 1, irrigation water contaminated.
+Ecoli_soil = np.random.normal(0.549,0.816) #log10 CFU/g
+P_Ecoli_Water = 0.35
+PosYN_Ecoli_Water = np.random.binomial(1,P_Ecoli_Water)
+C_Ecoli_Water = (10**np.random.normal(0.604,0.357))/10 #CFU/ml water
+
+Tr_Irr_Water = np.random.uniform(1.8,21.6) #ml/g produce
+Pr_Irr_Splashing = pert(0.02,0.04,0.05)
+Pr_Rain_Splashing = 1
+Soil_Transfer = betagen(0.4,0.8,0.05,16.4)
+P_Soil_Plant = np.random.uniform(0.35,0.9)
+
+Increase_Irrigation =C_Ecoli_Water*Tr_Irr_Water*PosYN_Ecoli_Water #CFU/g
+Increase_IrrigationCFU = Increase_Irrigation*(454*100_000)
         
+#Scenario 2 Rain splashing
+Increase_RainSplash = (10**Ecoli_soil)*Soil_Transfer*P_Soil_Plant*Pr_Rain_Splashing
+Increase_RainSplashCFU = Increase_RainSplash*(454*100_000)
+
+#Scenario 3 Irrigation water splash
+Increase_IrrigationSplash = (10**Ecoli_soil)*Soil_Transfer*P_Soil_Plant*Pr_Irr_Splashing
+Increase_IrrigationSplashCFU = Increase_IrrigationSplash *(454*100_000)
+
+#Scenario 4 Ferral Swine Defecation foliar irrigation: (Clustered Contamination)
+amount_ferralloc = 4260 #mass of feces a day #456 g
+amount_feralprec = 1_000 #g
+amount_cattleprec = 10_000 #g
+
+Cont_Ferral=np.random.lognormal(-1.26,2.2)
+Cont_Ferral_CFU=10**Cont_Ferral
+CFU_ferralloc_CFU = amount_ferralloc*Cont_Ferral_CFU
+print(CFU_ferralloc_CFU)
+
+def transfer_1(val):
+    if val<=0.619:
+        trans = 1.3*(val-0)/(0.619-0)
+    elif (val>=0.619) and (val<=0.946):
+        trans = 1.3+((340-1.3)*(val-0.619)/(0.946-0.619))
+    else:
+       trans = 340+((230000-340)*(val-0.946)/(1-0.946)) 
+    return trans
+
+#Transfer Ratio
+tr=transfer_1(np.random.uniform(0,1))/(1.29*10**8)
+print(tr)
+
+CFU_ferralloc_CFU*tr
+
+#Scenario 5 Ferral swine defectation
+
 
 
 
