d486d24a2a77c5207f8e5f8f4999789507764222
Gustavo Reyes <gareyes3@illinois.edu>
2022-06-29 17:19:36 -0500

    updates

--- Model/MainModel.py
+++ Model/MainModel.py
@@ -46,15 +46,36 @@ ContScen.F_systematic_C(df = df,
 df["Harvester"] = Funz.F_Assign_Harvesters(df = df, n_harvesters = T_Inputz.N_Harvesters)
 
 #%%
+#Basic Information
 Tomato_weight = 123/454 #for medium tomato
 Tomato_Sequence = int(35_000/0.27)
 Individual_Tomatoes = np.arange(1,Tomato_Sequence)
-Individual_Plants = np.repeat(np.arange(1,int(np.ceil(Tomato_Sequence/20))),20)
-Pick_Random = random.sample([1,2,3], len([1,2,3]))*(np.ceil(Tomato_Sequence/3))
+
+Individual_Plants = np.repeat(np.arange(1,int(np.ceil(Tomato_Sequence/120))),120+1)
+Pick_Sequence = [1,2,3]*(int(np.ceil(Tomato_Sequence/3)))
+Pick_Random = random.sample(Pick_Sequence, len(Pick_Sequence))
+
+#Harvest information
+Total_Harvesters = 14
+
+#Model
+Field_df=pd.DataFrame({"Tomato_ID": Individual_Tomatoes,
+                       "Plant_ID": Individual_Plants[0:Individual_Tomatoes.size],
+                       "Pick_ID": Pick_Random[0:Individual_Tomatoes.size],
+                       "Harvester" : 0,
+                       "CFU": 0,
+                       "Location": 1,
+                  })
+
+#Harvest
+
+Harvester_Pattern = np.repeat(list(range(1,15)),180)
+Harvester_Pattern_Full=np.tile(Harvester_Pattern,int(np.ceil(Tomato_Sequence/len(Harvester_Pattern))))
+count = 0
+for i in Field_df['Pick_ID'].to_numpy():
+    if i == 1:
+        Field_df.loc[count, "Harvester"] = Harvester_Pattern_Full[count]
+    count = count+1
 
 
 
-
-
-pd.DataFrame({""
-    })
