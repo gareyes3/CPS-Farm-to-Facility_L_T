e7683860ca2f9780b98279650f420e3b56e6243e
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2022-07-25 07:37:29 -0500

    chaNGES TO CC

--- Model/MainModel.py
+++ Model/MainModel.py
@@ -163,6 +163,59 @@ def applying_survival_salmonella_cucum(df, Time, RH, Temp):
     return df
 
 
+def Tomato_Wash(df):
+
+    FC_lvl = 150 #np.random.triangular(0,5,10)
+    if FC_lvl<10:
+        log_red_Wash_mean = (FC_lvl*0.04)+0.3
+    elif FC_lvl>10:
+        log_red_Wash_mean = (FC_lvl*0.0056)+0.5952
+        
+    logred_sd = 0.175
+    
+    logred_cont = np.random.normal(log_red_Wash_mean, logred_sd)
+    
+    #Transfer to uncontaminated pieces. 
+    Log_Trans_Upper = (-0.6798*FC_lvl)-0.6003
+    Log_Trans_Lower = (-1.3596*FC_lvl)-0.6
+    
+    Field_df_1 =df.loc[df["Location"]==2].copy() #location 4 is wash water
+    Field_df_1_conts = Field_df_1["CFU"].copy()
+    
+    Tras_old= 0
+    
+    for i in list(Field_df_1_conts.index):
+        #Parameters from https://www.sciencedirect.com/science/article/pii/S0963996916306081#f0010
+        #Maffei. 
+        #Backup https://www.sciencedirect.com/science/article/pii/S0740002019309694?casa_token=dt-V4Cbh89YAAAAA:FoQTt7f3Hly2-k9lJjUXPqkZ-W5tJChevsm2XIz3UTimqrYdUT9zAoqZUQlZWx1OhENvX3zqpqw
+        Cont = Field_df_1_conts[i]
+        
+        logred_cont = -np.random.normal(log_red_Wash_mean, logred_sd)
+        Percent_Trans = 10**np.random.uniform(Log_Trans_Lower,Log_Trans_Upper)
+        
+        if logred_cont>=0:
+            GrowthCeil = math.ceil(logred_cont)
+            Difference = logred_cont-GrowthCeil
+            MaxCont = Cont*10**GrowthCeil
+            Updated_CFUs = rng.binomial(MaxCont,10**Difference)
+        else:
+            Updated_CFUs= rng.binomial(Cont,10**logred_cont)
+    
+        Reduction = Updated_CFUs
+        
+        if i == 0:
+            Field_df_1_conts[i] = Reduction
+        if i >0:
+            Field_df_1_conts[i] = Reduction + Tras_old
+        Cont = Field_df_1_conts[i]
+        Transfer = rng.binomial(Cont,Percent_Trans)
+        Tras_old = Transfer
+        
+    Field_df_1.loc[:, "CFU"] = Field_df_1_conts
+    df.update(Field_df_1)
+    return df
+
+
 
 
 #%%%
@@ -274,7 +327,7 @@ for i in Days:
                                         No_Cont_Clusters = 1)
         print("Field Cont with Bird Dropping")
     
-    if i in PHS_Days:
+    #if i in PHS_Days:
         
     
     #This is where process starts, along with simulated harvest. 
@@ -294,7 +347,7 @@ for i in Days:
                                     Pick_No =Current_Pick  , 
                                     Cont_Harvester_No =Contam_Harvester )
             
-       #Bin Contmaination
+        #Bin Contmaination
         if (np.random.uniform(0,1)<Pr_Bin_cont):
             #Picking the contaminated harvester at random
             Contam_Bin = random.sample(list(range(1,Total_Bins+1)),1)[0]
@@ -334,6 +387,7 @@ for i in Days:
         
         #Processing
         #Wasing. 
+        Field_df= Tomato_Wash(df = Field_df)
         
 
 
--- Model/Washing Tomatoes CC.py
+++ Model/Washing Tomatoes CC.py
@@ -5,21 +5,72 @@ Created on Thu Jul 21 12:37:55 2022
 @author: gareyes3
 """
 import numpy as np
+import math
 #Washing FC 
-FC_lvl = np.random.triangular(0,80,250)
+
+10**np.random.uniform(Log_Trans_Lower,Log_Trans_Upper)
+
+
+Field_df.loc[list(range(100)),"CFU"] = 100000
+
+
+Field_df_1 = Field_df.loc[list(range(1000)),"CFU"]
+
+
+#def Tomato_Wash(df):
+
+FC_lvl = 150 #np.random.triangular(0,5,10)
 if FC_lvl<10:
-    log_red_Wash_mean = FC_lvl*0.04+0.3
+    log_red_Wash_mean = (FC_lvl*0.04)+0.3
 elif FC_lvl>10:
-    log_red_Wash_mean = FC_lvl*0.0056+0.5952
+    log_red_Wash_mean = (FC_lvl*0.0056)+0.5952
     
 logred_sd = 0.175
 
+logred_cont = np.random.normal(log_red_Wash_mean, logred_sd)
+
 #Transfer to uncontaminated pieces. 
-Log_Trans_Upper = -0.6798*FC_lvl-0.6
-Log_Trans_Lower = -1.3596*FC_lvl-0.6
+Log_Trans_Upper = (-0.6798*FC_lvl)-0.6003
+Log_Trans_Lower = (-1.3596*FC_lvl)-0.6
 
-np.random.uniform(Log_Trans_Lower,Log_Trans_Upper)
+Field_df_1 =Field_df.loc[Field_df["Location"]==2].copy() #location 4 is wash water
+Field_df_1_conts = Field_df_1["CFU"].copy()
+
+Tras_old= 0
 
 
-
+for i in list(Field_df_1_conts.index):
+    #Parameters from https://www.sciencedirect.com/science/article/pii/S0963996916306081#f0010
+    #Maffei. 
+    #Backup https://www.sciencedirect.com/science/article/pii/S0740002019309694?casa_token=dt-V4Cbh89YAAAAA:FoQTt7f3Hly2-k9lJjUXPqkZ-W5tJChevsm2XIz3UTimqrYdUT9zAoqZUQlZWx1OhENvX3zqpqw
     
+    Cont = Field_df_1_conts[i]
+    
+    logred_cont = -np.random.normal(log_red_Wash_mean, logred_sd)
+    Percent_Trans = 10**np.random.uniform(Log_Trans_Lower,Log_Trans_Upper)
+    
+    if logred_cont>=0:
+        GrowthCeil = math.ceil(logred_cont)
+        Difference = logred_cont-GrowthCeil
+        MaxCont = Cont*10**GrowthCeil
+        Updated_CFUs = rng.binomial(MaxCont,10**Difference)
+    else:
+        Updated_CFUs= rng.binomial(Cont,10**logred_cont)
+
+    Reduction = Updated_CFUs
+    
+    if i == 0:
+        Field_df_1_conts[i] = Reduction
+    if i >0:
+        Field_df_1_conts[i] = Reduction + Tras_old
+    Cont = Field_df_1_conts[i]
+    Transfer = rng.binomial(Cont,Percent_Trans)
+    Tras_old = Transfer
+    
+Field_df_1.loc[:, "CFU"] = Field_df_1_conts
+df.update(Field_df_1)
+    
+    return df
+    
+        
+Field_df= Tomato_Wash(df = Field_df)
