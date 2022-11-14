522460340883a14ca62a6cf63b896bb0e9162510
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2022-11-14 07:50:52 -0600

    updates to prot power

--- Model/Analysis Protective Power.py
+++ Model/Analysis Protective Power.py
@@ -519,3 +519,228 @@ for i in List_of_cont_day:
 Final_DF_ProtectiveP6 = pd.concat(List_Outputs)
 
 Final_DF_ProtectiveP6.to_csv(path_or_buf = "C:\\Users\\gareyes3\\Documents\\GitHub\\CPS-Farm-to-Facility_L_T\\Model\\Data_Tomato_Outputs\\PP10_6.csv")
+
+#%% 1% clusters
+#%%
+##### 10% cluster: 
+    
+#%%
+List_Outputs = []
+List_of_cont_day = list(range(1,43))
+for i in List_of_cont_day:
+    Inputz_T.Iteration_Number = 100
+    Scen_T.Tomatoes_per_sample = 2
+    Scen_T.Samp_Plan = 1
+    Scen_T.Cont_Scenario = 3
+    Scen_T.Samp_Method = 1
+    Scen_T.Chosen_cont_day = 1
+    Scen_T.Contamination_Day = i
+    reload(DepInputz)
+    Outs_S1_A = MainModel.Main_Loop()
+    
+    Rejected_Pick1 = Outs_S1_A[0]["CFU_Bef_Pick1PHS"] - Outs_S1_A[0]["CFU_Aft_Pick1PHS"]
+    Cont_Avail_Pick1 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick1PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick2 = Outs_S1_A[0]["CFU_Bef_Pick2PHS"] - Outs_S1_A[0]["CFU_Aft_Pick2PHS"]
+    Cont_Avail_Pick2 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick2PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick3 = Outs_S1_A[0]["CFU_Bef_Pick3PHS"] - Outs_S1_A[0]["CFU_Aft_Pick3PHS"]
+    Cont_Avail_Pick3 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick3PHS"]==0, "Yes", "No"))
+    
+    Total_Exposure = Outs_S1_A[0]["Total CFU"]
+    
+    Prot_Power=pd.concat(
+               [Outs_S1_A[0]["CFU_Bef_Pick1PHS"],Rejected_Pick1,Cont_Avail_Pick1,
+               Outs_S1_A[0]["CFU_Bef_Pick2PHS"],Rejected_Pick2,Cont_Avail_Pick2,
+               Outs_S1_A[0]["CFU_Bef_Pick3PHS"],Rejected_Pick3,Cont_Avail_Pick3,
+               Total_Exposure,pd.Series(Outs_S1_A[5])], axis = 1)
+    
+    List_Outputs.append(Prot_Power)
+    
+Final_DF_ProtectiveP = pd.concat(List_Outputs)
+
+Final_DF_ProtectiveP.to_csv(path_or_buf = "C:\\Users\\gareyes3\\Documents\\GitHub\\CPS-Farm-to-Facility_L_T\\Model\\Data_Tomato_Outputs\\PP10_1.csv")
+##############################################
+List_Outputs = []
+List_of_cont_day = list(range(1,43))
+for i in List_of_cont_day:
+    Inputz_T.Iteration_Number = 100
+    Scen_T.Tomatoes_per_sample = 20
+    Scen_T.Samp_Plan = 1
+    Scen_T.Cont_Scenario = 3
+    Scen_T.Samp_Method = 1
+    Scen_T.Chosen_cont_day = 1
+    Scen_T.Contamination_Day = i
+    reload(DepInputz)
+    Outs_S1_A = MainModel.Main_Loop()
+    
+    Rejected_Pick1 = Outs_S1_A[0]["CFU_Bef_Pick1PHS"] - Outs_S1_A[0]["CFU_Aft_Pick1PHS"]
+    Cont_Avail_Pick1 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick1PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick2 = Outs_S1_A[0]["CFU_Bef_Pick2PHS"] - Outs_S1_A[0]["CFU_Aft_Pick2PHS"]
+    Cont_Avail_Pick2 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick2PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick3 = Outs_S1_A[0]["CFU_Bef_Pick3PHS"] - Outs_S1_A[0]["CFU_Aft_Pick3PHS"]
+    Cont_Avail_Pick3 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick3PHS"]==0, "Yes", "No"))
+    
+    Total_Exposure = Outs_S1_A[0]["Total CFU"]
+    
+    Prot_Power=pd.concat(
+               [Outs_S1_A[0]["CFU_Bef_Pick1PHS"],Rejected_Pick1,Cont_Avail_Pick1,
+               Outs_S1_A[0]["CFU_Bef_Pick2PHS"],Rejected_Pick2,Cont_Avail_Pick2,
+               Outs_S1_A[0]["CFU_Bef_Pick3PHS"],Rejected_Pick3,Cont_Avail_Pick3,
+               Total_Exposure,pd.Series(Outs_S1_A[5])], axis = 1)
+    
+    List_Outputs.append(Prot_Power)
+    
+Final_DF_ProtectiveP2 = pd.concat(List_Outputs)
+
+Final_DF_ProtectiveP2.to_csv(path_or_buf = "C:\\Users\\gareyes3\\Documents\\GitHub\\CPS-Farm-to-Facility_L_T\\Model\\Data_Tomato_Outputs\\PP10_2.csv")
+
+#%%
+List_Outputs = []
+List_of_cont_day = list(range(1,43))
+for i in List_of_cont_day:
+    Inputz_T.Iteration_Number = 100
+    Scen_T.Tomatoes_per_sample = 6
+    Scen_T.Samp_Plan = 1
+    Scen_T.Cont_Scenario = 3
+    Scen_T.Samp_Method = 1
+    Scen_T.Chosen_cont_day = 1
+    Scen_T.Contamination_Day = i
+    reload(DepInputz)
+    Outs_S1_A = MainModel.Main_Loop()
+    
+    Rejected_Pick1 = Outs_S1_A[0]["CFU_Bef_Pick1PHS"] - Outs_S1_A[0]["CFU_Aft_Pick1PHS"]
+    Cont_Avail_Pick1 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick1PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick2 = Outs_S1_A[0]["CFU_Bef_Pick2PHS"] - Outs_S1_A[0]["CFU_Aft_Pick2PHS"]
+    Cont_Avail_Pick2 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick2PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick3 = Outs_S1_A[0]["CFU_Bef_Pick3PHS"] - Outs_S1_A[0]["CFU_Aft_Pick3PHS"]
+    Cont_Avail_Pick3 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick3PHS"]==0, "Yes", "No"))
+    
+    Total_Exposure = Outs_S1_A[0]["Total CFU"]
+    
+    Prot_Power=pd.concat(
+               [Outs_S1_A[0]["CFU_Bef_Pick1PHS"],Rejected_Pick1,Cont_Avail_Pick1,
+               Outs_S1_A[0]["CFU_Bef_Pick2PHS"],Rejected_Pick2,Cont_Avail_Pick2,
+               Outs_S1_A[0]["CFU_Bef_Pick3PHS"],Rejected_Pick3,Cont_Avail_Pick3,
+               Total_Exposure,pd.Series(Outs_S1_A[5])], axis = 1)
+    
+    List_Outputs.append(Prot_Power)
+    
+Final_DF_ProtectiveP3 = pd.concat(List_Outputs)
+
+Final_DF_ProtectiveP3.to_csv(path_or_buf = "C:\\Users\\gareyes3\\Documents\\GitHub\\CPS-Farm-to-Facility_L_T\\Model\\Data_Tomato_Outputs\\PP10_3.csv")
+##############################################
+List_Outputs = []
+List_of_cont_day = list(range(1,43))
+for i in List_of_cont_day:
+    Inputz_T.Iteration_Number = 100
+    Scen_T.Tomatoes_per_sample = 60
+    Scen_T.Samp_Plan = 1
+    Scen_T.Cont_Scenario = 3
+    Scen_T.Samp_Method = 1
+    Scen_T.Chosen_cont_day = 1
+    Scen_T.Contamination_Day = i
+    reload(DepInputz)
+    Outs_S1_A = MainModel.Main_Loop()
+    
+    Rejected_Pick1 = Outs_S1_A[0]["CFU_Bef_Pick1PHS"] - Outs_S1_A[0]["CFU_Aft_Pick1PHS"]
+    Cont_Avail_Pick1 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick1PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick2 = Outs_S1_A[0]["CFU_Bef_Pick2PHS"] - Outs_S1_A[0]["CFU_Aft_Pick2PHS"]
+    Cont_Avail_Pick2 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick2PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick3 = Outs_S1_A[0]["CFU_Bef_Pick3PHS"] - Outs_S1_A[0]["CFU_Aft_Pick3PHS"]
+    Cont_Avail_Pick3 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick3PHS"]==0, "Yes", "No"))
+    
+    Total_Exposure = Outs_S1_A[0]["Total CFU"]
+    
+    Prot_Power=pd.concat(
+               [Outs_S1_A[0]["CFU_Bef_Pick1PHS"],Rejected_Pick1,Cont_Avail_Pick1,
+               Outs_S1_A[0]["CFU_Bef_Pick2PHS"],Rejected_Pick2,Cont_Avail_Pick2,
+               Outs_S1_A[0]["CFU_Bef_Pick3PHS"],Rejected_Pick3,Cont_Avail_Pick3,
+               Total_Exposure,pd.Series(Outs_S1_A[5])], axis = 1)
+    
+    List_Outputs.append(Prot_Power)
+    
+Final_DF_ProtectiveP4 = pd.concat(List_Outputs)
+
+Final_DF_ProtectiveP4.to_csv(path_or_buf = "C:\\Users\\gareyes3\\Documents\\GitHub\\CPS-Farm-to-Facility_L_T\\Model\\Data_Tomato_Outputs\\PP10_4.csv")
+
+#%%
+List_Outputs = []
+List_of_cont_day = list(range(1,43))
+for i in List_of_cont_day:
+    Inputz_T.Iteration_Number = 100
+    Scen_T.Tomatoes_per_sample = 20
+    Scen_T.Samp_Plan = 1
+    Scen_T.Cont_Scenario = 3
+    Scen_T.Samp_Method = 2
+    Scen_T.N_Replicates = 20
+    Scen_T.Chosen_cont_day = 1
+    Scen_T.Contamination_Day = i
+    reload(DepInputz)
+    Outs_S1_A = MainModel.Main_Loop()
+    
+    Rejected_Pick1 = Outs_S1_A[0]["CFU_Bef_Pick1PHS"] - Outs_S1_A[0]["CFU_Aft_Pick1PHS"]
+    Cont_Avail_Pick1 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick1PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick2 = Outs_S1_A[0]["CFU_Bef_Pick2PHS"] - Outs_S1_A[0]["CFU_Aft_Pick2PHS"]
+    Cont_Avail_Pick2 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick2PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick3 = Outs_S1_A[0]["CFU_Bef_Pick3PHS"] - Outs_S1_A[0]["CFU_Aft_Pick3PHS"]
+    Cont_Avail_Pick3 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick3PHS"]==0, "Yes", "No"))
+    
+    Total_Exposure = Outs_S1_A[0]["Total CFU"]
+    
+    Prot_Power=pd.concat(
+               [Outs_S1_A[0]["CFU_Bef_Pick1PHS"],Rejected_Pick1,Cont_Avail_Pick1,
+               Outs_S1_A[0]["CFU_Bef_Pick2PHS"],Rejected_Pick2,Cont_Avail_Pick2,
+               Outs_S1_A[0]["CFU_Bef_Pick3PHS"],Rejected_Pick3,Cont_Avail_Pick3,
+               Total_Exposure,pd.Series(Outs_S1_A[5])], axis = 1)
+    
+    List_Outputs.append(Prot_Power)
+    
+Final_DF_ProtectiveP5 = pd.concat(List_Outputs)
+
+Final_DF_ProtectiveP5.to_csv(path_or_buf = "C:\\Users\\gareyes3\\Documents\\GitHub\\CPS-Farm-to-Facility_L_T\\Model\\Data_Tomato_Outputs\\PP10_5.csv")
+##############################################
+List_Outputs = []
+List_of_cont_day = list(range(1,43))
+for i in List_of_cont_day:
+    Inputz_T.Iteration_Number = 100
+    Scen_T.Tomatoes_per_sample = 60
+    Scen_T.Samp_Plan = 1
+    Scen_T.Cont_Scenario = 3
+    Scen_T.Samp_Method = 2
+    Scen_T.N_Replicates = 20
+    Scen_T.Chosen_cont_day = 1
+    Scen_T.Contamination_Day = i
+    reload(DepInputz)
+    Outs_S1_A = MainModel.Main_Loop()
+    
+    Rejected_Pick1 = Outs_S1_A[0]["CFU_Bef_Pick1PHS"] - Outs_S1_A[0]["CFU_Aft_Pick1PHS"]
+    Cont_Avail_Pick1 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick1PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick2 = Outs_S1_A[0]["CFU_Bef_Pick2PHS"] - Outs_S1_A[0]["CFU_Aft_Pick2PHS"]
+    Cont_Avail_Pick2 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick2PHS"]==0, "Yes", "No"))
+    
+    Rejected_Pick3 = Outs_S1_A[0]["CFU_Bef_Pick3PHS"] - Outs_S1_A[0]["CFU_Aft_Pick3PHS"]
+    Cont_Avail_Pick3 = pd.Series(np.where(Outs_S1_A[0]["CFU_Bef_Pick3PHS"]==0, "Yes", "No"))
+    
+    Total_Exposure = Outs_S1_A[0]["Total CFU"]
+    
+    Prot_Power=pd.concat(
+               [Outs_S1_A[0]["CFU_Bef_Pick1PHS"],Rejected_Pick1,Cont_Avail_Pick1,
+               Outs_S1_A[0]["CFU_Bef_Pick2PHS"],Rejected_Pick2,Cont_Avail_Pick2,
+               Outs_S1_A[0]["CFU_Bef_Pick3PHS"],Rejected_Pick3,Cont_Avail_Pick3,
+               Total_Exposure,pd.Series(Outs_S1_A[5])], axis = 1)
+    
+    List_Outputs.append(Prot_Power)
+    
+Final_DF_ProtectiveP6 = pd.concat(List_Outputs)
+
+Final_DF_ProtectiveP6.to_csv(path_or_buf = "C:\\Users\\gareyes3\\Documents\\GitHub\\CPS-Farm-to-Facility_L_T\\Model\\Data_Tomato_Outputs\\PP10_6.csv")
